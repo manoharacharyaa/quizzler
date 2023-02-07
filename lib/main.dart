a9301d2688142c7c7be44415d03502e68866af30
Manohar <manoharacharya63@gmail.com>
2023-02-07 12:49:05 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Quizzler());
+}
+
+class Quizzler extends StatelessWidget {
+  const Quizzler({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.grey[800],
+          title: Text(
+            'Quizzler',
+            style: TextStyle(
+              fontFamily: 'EuropaNuova',
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        //TODO: Questions
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Center(
+              child: Text(
+                'All Your Questions Goes Here',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'EuropaNuova',
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+        ),
+        //TODO: True Button
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.green,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                elevation: 100,
+              ),
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'EuropaNuova',
+                  fontSize: 25,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+              onPressed: () {
+                // user picks True
+              },
+            ),
+          ),
+        ),
+        //TODO: False Button
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.red,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                elevation: 100,
+              ),
+              child: Text(
+                'False',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'EuropaNuova',
+                  fontSize: 25,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+              onPressed: () {
+                //User picks False
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
