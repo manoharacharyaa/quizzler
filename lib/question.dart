e32a213e65b85d42172beedc86e55375b0ed70bc
Manohar <manoharacharya63@gmail.com>
2023-02-18 21:31:13 +0530

    14th reducing the code of constructor by adding .this in questions.dart

--- lib/question.dart
+++ lib/question.dart
@@ -1,10 +1,6 @@
 class Question {
-  String questionText = 'q'; //provides some random (q & true) values
-  bool questionAnswer = true; //because they can't have null values
+  String questionText;
+  bool questionAnswer;
 
-  Question(String q, bool a) {
-    //it helps in grouping question & answer
-    questionText = q;
-    questionAnswer = a;
-  }
+  Question(this.questionText, this.questionAnswer);
 }
